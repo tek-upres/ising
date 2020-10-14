d0ba457810f279271f67db98a6dddf2faf935a95
arfaoui-sami <sami.arfaoui@tek-up.tn>
2020-10-14 02:46:30 +0100

    auth

--- app/src/main/java/com/pi/ising/MainActivity.java
+++ app/src/main/java/com/pi/ising/MainActivity.java
@@ -1,22 +1,83 @@
 package com.pi.ising;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText mEmailfield;
+    private EditText mPasswordfield;
+    private Button mloginbtn;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        mEmailfield = (EditText) findViewById(R.id.Emailfiled);
+        mPasswordfield = (EditText) findViewById(R.id.Passwordfiled);
+        mloginbtn = (Button) findViewById(R.id.loginbnt);
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() != null) {
+                    startActivity(new Intent(MainActivity.this, AccountActivity.class));
+                    finish();
+                }
+            }
+        };
+        mloginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSingIn();
+            }
+        });
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    private void startSingIn() {
+        String email = mEmailfield.getText().toString();
+        String password = mPasswordfield.getText().toString();
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Toast.makeText(MainActivity.this, "Filed is empty", Toast.LENGTH_LONG).show();
+        } else {
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (!task.isSuccessful()) {
+                        Toast.makeText(MainActivity.this, "Sign in Problem", Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
+
+    }
 
 }
+
+
+
